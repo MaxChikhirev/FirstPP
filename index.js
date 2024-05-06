f047d74cb7a6711ee543f76e597bd9540c560e42
Max Ch <windomia2@mail.com>
2024-05-06 22:36:55 +0500

    add folder structure and pages

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+app.use('/src/pages', express.static(path.join(__dirname, 'public/src/pages')));
+app.use('/styles', express.static(path.join(__dirname, 'public/styles')));
+app.use('/scripts', express.static(path.join(__dirname, 'public/scripts')));
+app.use(bodyParser.json());
+
+const products = [
+   {
+      id: 1,
+      name: 'phone',
+      price: 300
+   },
+   {
+      id: 2,
+      name: 'tablet',
+      price: 700
+   }
+];
+
+app.get('/', (req, res) => {
+   res.send('Hello World!');
+});
+app.get('/products', (req,res) => res.json(products));
+app.post('/products', (req,res) => {
+   products.push(req.body);
+   res.json(req.body);
+});
+
+
+
+app.get('/', (req, res) => res.send('Привет мир123!'));
+
+app.listen (3000, () => console.log ('Listening on port 3000...'));
+
 const db = require('./dbConfig');
 
 db.query('SELECT NOW()', (err, res) => {
---
+++ my_online_shop/public/scripts/login_scripts.js
@@ -0,0 +1,5 @@
+document.querySelector('.form-btn').addEventListener('click', function() {
+    document.querySelector('.login-form').classList.toggle('active');
+    document.querySelector('.signup-form').classList.toggle('active');
+    document.querySelector('.slider-tab').classList.toggle('active');
+});
